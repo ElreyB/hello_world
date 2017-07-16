ad09ad200a075fb373e0b1b5b29e97df789d8645
ElreyB <elbelmonti@gmail.com>
2017-07-16 14:18:51 -0700

    write multiply lines to the console

---
+++ Multi_lines.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MultiLine
+{
+  static void Main()
+  {
+    Console.WriteLine("Tell what you want,");
+    Console.WriteLine("What you really, really want.");
+    Console.WriteLine("I'll tell you what I want,");
+    Console.WriteLine("What I really, really want.");
+  }
+}
